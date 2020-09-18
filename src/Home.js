6c9c28632a669a8d5b8821a46e7300f4a4e4ec22
Brenson <brensonw300@gmail.com>
2020-09-18 12:38:11 -0700

    Looking through code

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,8 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="home">
-          
-                <h1 style={{textAlign: "center", color: "#d62828", backgroundColor: "#fcbf49", textShadow: "2px 2px black", border: '3px solid black'}}>For An Experience That Is Out Of This World Call Lambda Eats</h1>
+                     
+                <h1 style={{textAlign: "center", color: "#d62828", backgroundColor: "#fcbf49", textShadow: "2px 2px black", border: '3px solid black'}}>For An  Out Of This World Experiance Call Lambda Eats</h1>
         </div>
     )
 }
